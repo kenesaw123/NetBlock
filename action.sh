923314915c57343677cb86f93e0db79b65872897
MONA <121730102+MeowDump@users.noreply.github.com>
2026-03-11 14:10:01 +0530

    Add refresh trigger

--- action.sh
+++ action.sh
@@ -1,1 +1,14 @@
 #!/system/bin/sh
+MODDIR=${0%/*}
+
+echo "
+    ____  _____   ________
+   / __ \/  _/ | / / ____/
+  / /_/ // //  |/ / / __  
+ / ____// // /|  / /_/ /  
+/_/   /___/_/ |_/\____/   
+                          
+"
+echo "- Refreshing NetBlock rules..."
+sh "$MODDIR/service.sh" &
+echo "- Refresh triggered ✓"
